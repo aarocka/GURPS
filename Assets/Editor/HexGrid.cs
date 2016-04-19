07ac4e57e758dc70ada9088aebe604eccddf038f
Aaron Kolker <akolker3@gmail.com>
2016-04-19 16:25:35 -0400

    Got hexagons to render at runtime

--- Assets/Editor/HexGrid.cs
+++ Assets/Editor/HexGrid.cs
@@ -2,11 +2,16 @@
 using UnityEditor;
 using System.Collections;
 public class HexGrid : EditorWindow {
+	int gridWidth;
+	int gridHeight;
+
 	[MenuItem ("Window/Hex Creator")]
 	public static void  ShowWindow () {
 		EditorWindow.GetWindow(typeof(HexGrid));
 	}
 	void OnGUI(){
-		
+		GUILayout.Label ("Field Size", EditorStyles.boldLabel);
+		gridWidth = EditorGUILayout.IntField ("Field Width", gridWidth);
+		gridWidth = EditorGUILayout.IntField ("Field Height", gridHeight);
 	}
 }
