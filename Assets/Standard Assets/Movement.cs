e9da55178dedfd409f50686bab773ecdb734e67f
Jacob Ross <jacobjr3000@aol.com>
2016-04-19 02:00:35 -0400

    Movement
    Created movement of an object with the arrow keys

---
+++ Assets/Standard Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+		void Update ()
+		{
+		
+
+			//Left movement
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Vector3 position = this.transform.position;
+			//prevents going off the board in x direction
+			if (position.x != -1) {
+				position.x--;
+				this.transform.position = position;
+			}
+		}
+			//right movement
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				Vector3 position = this.transform.position;
+			if (position.x != +1) {
+				position.x++;
+				this.transform.position = position;
+			}
+			}
+
+			//up movement
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				Vector3 position = this.transform.position;
+			if (position.z != +1) {
+				position.z++;
+				this.transform.position = position;
+			}
+			}
+			//down movement
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				Vector3 position = this.transform.position;
+			if (position.z != -1) {
+				position.z--;
+				this.transform.position = position;
+			}
+			}
+
+		}
+	}
