409985f6c3528ef5c241a484dc9fc2edc863ef00
Aaron Kolker <akolker3@gmail.com>
2016-04-19 17:05:16 -0400

    Fixed size fields, added text field, added button

--- Assets/Editor/HexGrid.cs
+++ Assets/Editor/HexGrid.cs
@@ -2,16 +2,23 @@
 using UnityEditor;
 using System.Collections;
 public class HexGrid : EditorWindow {
-	int gridWidth;
-	int gridHeight;
+	int gridWidth = 2;
+	int gridHeight = 2;
+	GameObject prefabTemplate;
+	string mapObjectName = "";
 
+	public int hiworld= 1;
 	[MenuItem ("Window/Hex Creator")]
 	public static void  ShowWindow () {
 		EditorWindow.GetWindow(typeof(HexGrid));
 	}
 	void OnGUI(){
-		GUILayout.Label ("Field Size", EditorStyles.boldLabel);
+		GUILayout.Label ("Hex Grid Creator", EditorStyles.boldLabel);
+		mapObjectName = EditorGUILayout.TextField ("Text Field", mapObjectName);
 		gridWidth = EditorGUILayout.IntField ("Field Width", gridWidth);
-		gridWidth = EditorGUILayout.IntField ("Field Height", gridHeight);
+		gridHeight = EditorGUILayout.IntField ("Field Height", gridHeight);
+		if (GUILayout.Button ("Hello")) {
+			Debug.Log ("Creating Grid Size " +gridWidth+"x"+gridHeight);
+		}
 	}
 }
