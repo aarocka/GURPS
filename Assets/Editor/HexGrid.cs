a0270dd40ba92a7b4b0619617e7eef4f9769f1cb
Aaron Kolker <akolker3@gmail.com>
2016-04-19 15:30:13 -0400

    started HexCreator Script

---
+++ Assets/Editor/HexGrid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+public class HexGrid : EditorWindow {
+	[MenuItem ("Window/Hex Creator")]
+	public static void  ShowWindow () {
+		EditorWindow.GetWindow(typeof(HexGrid));
+	}
+	void OnGUI(){
+		
+	}
+}
