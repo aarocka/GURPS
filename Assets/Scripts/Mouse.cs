835eed8061f16d3452e09efab0c1f1342c3729ac
Aaron Kolker <akolker3@gmail.com>
2016-04-27 20:48:16 -0400

    Added a script that handles mouse management

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //TODO handle touch screen
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(mouseRay, out hitInfo) ){
+            GameObject mousedOverObj = hitInfo.collider.transform.gameObject;
+            Debug.Log("Mouse is over something" + hitInfo.collider.transform.parent.name);
+
+            if (Input.GetButton(0))
+            {
+               //some stubbed function for attacking things i guess
+            }
+
+            //Move player to selected tile
+            if (Input.GetMouseButton(1))
+            {
+                Debug.Log("U clikd");
+            }
+
+        }
+	}
+}
