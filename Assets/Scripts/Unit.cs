d8374a11a98f2f0cc19657277452145a43c82b77
Aaron Kolker <akolker3@gmail.com>
2016-04-27 21:04:37 -0400

    Added code to move unit to a destination coord

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Unit : MonoBehaviour {
+public class Unit : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Vector3 destination;
+
+    float speed = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        destination = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move towards our destination
+
+        Vector3 dir = destination - transform.position;
+        Vector3 velocity = dir.normalized * speed * Time.deltaTime;
+
+        // Make sure the velocity doesn't actually exceed the distance we want.
+        velocity = Vector3.ClampMagnitude(velocity, dir.magnitude);
+
+        transform.Translate(velocity);
+
+    }
+
+
 }
