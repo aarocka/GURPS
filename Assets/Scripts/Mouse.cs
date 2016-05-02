be211c269da31afbb70b91433ff04166b58d77c0
Jacob Ross <jacobjr3000@aol.com>
2016-05-02 13:09:30 -0400

    Improved movement
    Made it so a player can only move one tile at a time up to five tiles.
    Need to implement being able to go back a tile without increasing the
    move counter.

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Mouse : MonoBehaviour {
     Unit player;
-    // Use this for initialization
+	int moveCounter=0;
+
+	// Use this for initialization
     void Start () {
         //Finds a gameobject that is tagged with player
         //We now have access to class unit
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Unit>();
-    }
+
+		}
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,33 +26,39 @@ public class Mouse : MonoBehaviour {
             GameObject mousedOverObj = hitInfo.collider.transform.parent.gameObject;
             //Debug.Log("Are hovering over" + hitInfo.collider.transform.parent.name);
 
-            if (Input.GetMouseButton(0))
-            {
-               //some stubbed function for attacking things i guess
-            }
 
-            //Move player to selected tile
-            if (Input.GetMouseButton(1))
-            {
-                //if we right click a hex
-                if (mousedOverObj.GetComponent<Hex>() != null)
-                {
-                    /* Ideally we should be passing the unit script the tile we selected
+				if (Input.GetMouseButton (0)) {
+					//some stubbed function for attacking things i guess
+				}
+
+				//Move player to selected tile
+				if (Input.GetMouseButtonUp (1)) {
+					//if we right click a hex
+					if (mousedOverObj.GetComponent<Hex> () != null ) {
+						/* Ideally we should be passing the unit script the tile we selected
                      * rather than it's coord's. This way it might be easier to implement
                      * pathfinding or only allow movement to adjecent tiles. That kind of
                      * logic would be handled through the Unit script.
                      */
-                    Debug.Log("U right clicked a hex tile");
-                    player.destination = mousedOverObj.transform.position;
-                }
-                //if we right click a unit
-                else if (mousedOverObj.GetComponent<Unit>()!=null)
-                {
-                    Debug.Log("You right clicked a unit");
-                }
-            }
 
+						Debug.Log ("You have right clicked a hex tile");
+					if( moveCounter < 5) {
+						if ((mousedOverObj.transform.position.x - player.destination.x <= 0.8821f &&
+							mousedOverObj.transform.position.z - player.destination.z <= 0.7641f) &&
+							(mousedOverObj.transform.position.x - player.destination.x >= -0.8821f &&
+								mousedOverObj.transform.position.z - player.destination.z >= -0.7641f)) {
+							//moves player to new tile
+							player.destination = mousedOverObj.transform.position;
+
+							moveCounter++;
+						}
+					}
+					} 
+				}
+                //if we right click a unit
+                else if (mousedOverObj.GetComponent<Unit> () != null) {
+						Debug.Log ("You right clicked a unit");
+					}
+				}
         }
 	}
-   
-}
