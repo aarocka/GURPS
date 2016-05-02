8154aa05a0d86a7bf80b36929d0ed6693b7351cb
Jacob Ross <jacobjr3000@aol.com>
2016-05-02 13:09:30 -0400

    Improved movement
    Made it so a player can only move one tile at a time up to five tiles.
    Need to implement being able to go back a tile without increasing the
    move counter.

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -16,7 +16,7 @@ public class Hex : MonoBehaviour {
 
 		// The problem is that our neighbours to our upper-left and upper-right
 		// might be at x-1 and x, OR they might be at x and x+1, depending
-		// on whether we our on an even or odd row (i.e. if y % 2 is 0 or 1)
+		// on whether we are on an even or odd row (i.e. if y % 2 is 0 or 1)
 
 		return null;
 	}
