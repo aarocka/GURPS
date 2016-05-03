e0930121d36e5d61314716405cfaf12f7802a2a1
Jacob Ross <jacobjr3000@aol.com>
2016-05-03 00:40:25 -0400

    created an actionTracker Class

---
+++ Assets/Scripts/ActionTracker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionTracker {
+	bool hasAttacked{ get; set;}
+	int dmgDealt{ get; set;} 
+	int playerHealth{ get; set;}
+	float[] move = new float[2];
+
+
+	public ActionTracker ()
+	{
+		hasAttacked=false;
+		dmgDealt=0; 
+		playerHealth=0;
+		move[0] = 0.0f;
+		move [1] = 0.0f;
+	}
+
+	public float[] getMove()
+	{
+		return move; 
+	}
+	public void setMove(float[] newMove)
+	{
+		this.move = newMove;
+	}
+}
