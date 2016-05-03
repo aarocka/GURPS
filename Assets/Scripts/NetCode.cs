443db351f3c59927cf3df4a1bf1702c22cd4be6c
Aaron Kolker <akolker3@gmail.com>
2016-05-02 20:34:08 -0400

    something

---
+++ Assets/Scripts/NetCode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class NetCode : MonoBehaviour {
+    private SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+        socket.Emit("join", "aaron");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void spawnEnimies()
+    {
+
+    }
+}
