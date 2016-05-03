e3db7d3d3c9fba865130eedf7d00fe2038fb7793
Aaron Kolker <akolker3@gmail.com>
2016-05-02 23:19:26 -0400

    got spawn Enemies logic mostly complete

--- Assets/Scripts/NetCode.cs
+++ Assets/Scripts/NetCode.cs
@@ -5,8 +5,8 @@ using SocketIO;
 public class NetCode : MonoBehaviour {
     private SocketIOComponent socket;
     public string nic = "";
-    static string gameInfoString = "{\"gameID\":123456,\"turn\":1,\"gameStart\":true,\"players\":[{\"uid\":\"#NrJ6wYR9JK_IBFCGAAAA\",\"playerNumber\":1,\"nicname\":\"Aaron\",\"posX\":0,\"posY\":0,\"maxHealth\":100,\"health\":100},{\"uid\":\"#NrJ6wYR9JK_IBFCGAAAA\",\"playerNumber\":2,\"nicname\":\"steve\",\"posX\":17.64,\"posY\":0,\"maxHealth\":100,\"health\":100}]}";
-    static string playerObjectString = "{\"uid\":\"#NrJ6wYR9JK_IBFCGAAAA\",\"playerNumber\":1,\"nicname\":\"Aaron\",\"posX\":0,\"posY\":0,\"maxHealth\":100,\"health\":100}";
+    static string gameInfoString = "{\"gameID\":123456,\"turn\":1,\"gameStart\":true,\"players\":[{\"uid\":\"#NQwErTy\",\"playerNumber\":1,\"nicname\":\"Aaron\",\"posX\":0,\"posY\":0,\"maxHealth\":100,\"health\":100},{\"uid\":\"#NrJ6wYR9JK_IBFCGAAAA\",\"playerNumber\":2,\"nicname\":\"steve\",\"posX\":17.64,\"posY\":0,\"maxHealth\":100,\"health\":100}]}";
+    static string playerObjectString = "{\"uid\":\"#NQwErTy\",\"playerNumber\":1,\"nicname\":\"Aaron\",\"posX\":0,\"posY\":0,\"maxHealth\":100,\"health\":100}";
     JSONObject gameInfo = new JSONObject(gameInfoString);
     JSONObject playerObject = new JSONObject(playerObjectString);
 
@@ -14,16 +14,12 @@ public class NetCode : MonoBehaviour {
     void Start () {
         GameObject go = GameObject.Find("SocketIO");
         socket = go.GetComponent<SocketIOComponent>();
-        socket.On("open", OnSocketOpen);
-        socket.On("playerJoined", spawnPlayer);
-        socket.On("gameStarted", spawnEnemies);
-
-        Debug.Log(playerObject.list.Count);
-
-        for (int i = 0; i < playerObject.list.Count; i++)
-        {
-            Debug.Log(playerObject.keys[i] +":"+ playerObject.list[i]);
-        }
+        //socket.On("open", OnSocketOpen);
+        //socket.On("playerJoined", spawnPlayer);
+        //socket.On("gameStarted", spawnEnemies);
+        spawnPlayer(playerObject);
+        spawnEnemies(gameInfo);
+        
 
     }
 	
@@ -41,10 +37,27 @@ public class NetCode : MonoBehaviour {
         }
 	}
 
-    void spawnEnemies(SocketIOEvent e)
+    void spawnEnemies(JSONObject e)
     {
-        Debug.Log(string.Format("[name: {0}, data: {1}]", e.name, e.data));
-        
+        //Debug.Log(string.Format("[name: {0}, data: {1}]", e.name, e.data));
+        JSONObject players = (JSONObject)e.list[3];
+        //Debug.Log(players.list[0]);
+
+        foreach (JSONObject j in players.list)
+        {
+
+            Debug.Log(j.list[1]);
+            Debug.Log(playerObject.list[1]);
+            if (j.list[1].n == playerObject.list[1].n)
+            {
+                Debug.Log("This one is the player");
+            }
+            else
+            {
+                Debug.Log("spawned the enemy");
+            }
+        }
+
     }
 
     void startGame()
@@ -52,10 +65,15 @@ public class NetCode : MonoBehaviour {
         socket.Emit("start", "something");
     }
 
-    public void spawnPlayer(SocketIOEvent e)
+    public void spawnPlayer(JSONObject e)
     {
-        Debug.Log(string.Format("[name: {0}, data: {1}]", e.name, e.data));
-        
+        //Debug.Log(string.Format("[name: {0}, data: {1}]", e.name, e.data));
+        Debug.Log("You are player number" + e.list[1]);
+        Debug.Log(e.keys[3] + ":" +e.list[3].n);
+        Debug.Log(e.keys[4] + ":" + e.list[4].n);
+        int tempX = (int)e.list[3].n;
+        int tempY = (int)e.list[4].n;        
+
     }
     public void OnSocketOpen(SocketIOEvent ev)
     {
