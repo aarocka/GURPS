179dc77ac140c3a8ea40fe75b77be780711b5569
vlarg10 <mbrown9419@gmail.com>
2016-06-09 01:11:55 -0400

    Created generateHexGrid function in hex grid

--- Assets/Editor/HexGrid.cs
+++ Assets/Editor/HexGrid.cs
@@ -13,50 +13,53 @@ public class HexGrid : EditorWindow {
 
 
 	public int hiworld= 1;
+
 	[MenuItem ("Window/Hex Creator")]
+
 	public static void  ShowWindow () {
 		EditorWindow.GetWindow(typeof(HexGrid));
 	}
+
 	void OnGUI(){
-		GUILayout.Label ("Hex Grid Creator", EditorStyles.boldLabel);
-		parentName = EditorGUILayout.TextField ("Text Field", parentName);
-		hexPrefab = EditorGUILayout.ObjectField(hexPrefab, typeof(GameObject), true);
-		gridWidth = EditorGUILayout.IntField ("Field Width", gridWidth);
-		gridHeight = EditorGUILayout.IntField ("Field Height", gridHeight);
-				
-		if (GUILayout.Button ("Generate")) {
-			Debug.Log ("Creating Grid Size " +gridWidth+"x"+gridHeight);
-		
-			for (int x = 0; x < gridWidth; x++) {
-				for (int y = 0; y < gridHeight; y++) {
+		generateHexGrid ();
 
-					float xPos = x * xOffset;
 
-					// Are we on an odd row?
-					if( y % 2 == 1 ) {
-						xPos += xOffset/2f;
-					}
+	}
+	private void generateHexGrid (){
+	GUILayout.Label ("Hex Grid Creator", EditorStyles.boldLabel);
+	parentName = EditorGUILayout.TextField ("Text Field", parentName);
+	hexPrefab = EditorGUILayout.ObjectField(hexPrefab, typeof(GameObject), true);
+	gridWidth = EditorGUILayout.IntField ("Field Width", gridWidth);
+	gridHeight = EditorGUILayout.IntField ("Field Height", gridHeight);
 
-					GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3( xPos,0, y * zOffset  ), Quaternion.identity  );
+	if (GUILayout.Button ("Generate")) {
+		Debug.Log ("Creating Grid Size " +gridWidth+"x"+gridHeight);
 
-					// Name the gameobject something sensible.
-					hex_go.name = "Hex_" + x + "_" + y;
+		for (int x = 0; x < gridWidth; x++) {
+			for (int y = 0; y < gridHeight; y++) {
 
-					// Make sure the hex is aware of its place on the map
-					hex_go.GetComponent<Hex>().x = x;
-					hex_go.GetComponent<Hex>().y = y;
+				float xPos = x * xOffset;
 
-					// TODO parent generated hex tiles to a game object of name parentName
-					//hex_go.transform.SetParent(this.transform);
-
-					// TODO: Quill needs to explain different optimization later...
-					hex_go.isStatic = true;
-
+				// Are we on an odd row?
+				if( y % 2 == 1 ) {
+					xPos += xOffset/2f;
 				}
-			}
 
+				GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3( xPos,0, y * zOffset  ), Quaternion.identity  );
 
-		}
+				// Name the gameobject something sensible.
+				hex_go.name = "Hex_" + x + "_" + y;
+
+				// Make sure the hex is aware of its place on the map
+				hex_go.GetComponent<Hex>().x = x;
+				hex_go.GetComponent<Hex>().y = y;
+
+				// TODO parent generated hex tiles to a game object of name parentName
+				//hex_go.transform.SetParent(this.transform);
+
+				// TODO: Quill needs to explain different optimization later...
+				hex_go.isStatic = true;
+
 	}
 
 						
