98a33bb9f471a6cfaedc967cf212b4dd74f3b5bb
vlarg10 <mbrown9419@gmail.com>
2016-06-25 19:14:28 -0400

    Added moveable overhead camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	public int speed;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +11,16 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (Vector3.left * Time.deltaTime * speed);
+
+		if (Input.GetKey(KeyCode.W))
+			transform.Translate (Vector3.up * Time.deltaTime * speed);
+
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate (Vector3.right * Time.deltaTime * speed);
+
+		if (Input.GetKey(KeyCode.S))
+			transform.Translate (Vector3.down * Time.deltaTime * speed);
 	}
 }
